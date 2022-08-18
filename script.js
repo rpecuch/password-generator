870df498161938341da8471c845f7e24ab32b68b
userrp <109092365+rpecuch@users.noreply.github.com>
2022-08-17 22:37:22 -0400

    added function to generate random password and display in a window

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Assignment plan:
-// user clicks button which runs the function
-// user prompted to select length of password 
-  // length between 8 and 128 char
-// user prompted to select types of characters to include (series of prompts)
-  // lowercase
-  // uppercase
-  // numeric
-  // special characters
-  // required to select at least one type
-  // input validated after each prompt
-// password is generated either in alert or written on page
-
 var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T","U","V","W","X","Y","Z"];
 var numbers = [1,2,3,4,5,6,7,8,9];
@@ -18,63 +5,60 @@ var specialCharacters = ["!","@","$","%","^","&","*","(",")"];
 var availableChar = [];
 
 function generatePassword() {
-  var length = prompt("Select length of password in characters");
+  var length = prompt("Select length of password in characters (at least 8 and no more than 128):");
   if(length >= 8 && length < 128) {
-    var includeLower = prompt("Would you like to include lowercase letters?").toUpperCase();
+    var includeLower = prompt("Would you like to include lowercase letters? Type 'Yes' or 'No'.").toUpperCase();
     if (includeLower === "YES") {
       availableChar = availableChar.concat(lowerLetters);
-      alert("You chose to include lowercase letters");
+      alert("You chose to include lowercase letters!");
     }
     else {
-      alert("You chose not to include lowercase letters");
+      alert("You chose not to include lowercase letters.");
     }
   
-    var includeUpper = prompt("Would you like to include uppercase letters?").toUpperCase();
+    var includeUpper = prompt("Would you like to include uppercase letters? Type 'Yes' or 'No'.").toUpperCase();
     if(includeUpper === "YES")  {
       availableChar = availableChar.concat(upperLetters);
-      alert("You chose to include uppercase letters");
+      alert("You chose to include uppercase letters!");
     }
     else {
-      alert("You chose not to include uppercase letters");
+      alert("You chose not to include uppercase letters.");
     }
   
-    var includeNumber = prompt("Would you like to include numbers?").toUpperCase();
+    var includeNumber = prompt("Would you like to include numbers? Type 'Yes' or 'No'.").toUpperCase();
     if(includeNumber === "YES") {
       availableChar = availableChar.concat(numbers);
-      alert("You chose to include numbers");
+      alert("You chose to include numbers!");
     }
     else {
-      alert("You chose not to include numbers");
+      alert("You chose not to include numbers.");
     }
   
-    var includeSpecial = prompt("Would you like to include special characters?").toUpperCase();
+    var includeSpecial = prompt("Would you like to include special characters? Type 'Yes' or 'No'.").toUpperCase();
     if(includeSpecial === "YES") {
       availableChar = availableChar.concat(specialCharacters);
-      alert("You chose to include special characters");
+      alert("You chose to include special characters!");
     }
     else {
-      alert("You chose not to include special characters");
+      alert("You chose not to include special characters.");
     }
 
-    // now actually generate the password
-
-    // var emptyString = "";
-    var randomChar = availableChar[Math.floor(Math.random() * availableChar.length)];
-    console.log(randomChar);
-
-    alert("This is your password: " + randomChar + randomChar);
-
-    // for(var i=0;i<length;i++) {
-    //   emptyString + randomChar;
-    //   console.log(randomChar);
-    // }
-
-    // console.log(emptyString);
-
-
+    if(includeLower === "NO" && includeUpper === "NO" && includeNumber === "NO" && includeSpecial === "NO") {
+      alert ("Error, please answer 'Yes' to one of the questions.");
+    }
+    else {
+      function generateString() {
+        var emptyString = "";
+        for(var i=0;i<length;i++) {
+        emptyString += availableChar[Math.floor(Math.random() * availableChar.length)];
+      } 
+        alert("This is your password: " + emptyString);
+      }
+      generateString();
+    }
   }
   else {
-    alert("Invalid input");
+    alert("Invalid input. Please select a length of at least 8 characters and no more than 128 characters.");
   }
 }
 
