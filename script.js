4ef169a9b4130bb9b80c4d7410f35f81cb5a8da6
userrp <109092365+rpecuch@users.noreply.github.com>
2022-08-17 23:15:35 -0400

    added special character choices

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T","U","V","W","X","Y","Z"];
 var numbers = [1,2,3,4,5,6,7,8,9];
-var specialCharacters = ["!","@","$","%","^","&","*","(",")"];
+var specialCharacters = ["!","@","$","%","^","&","*","(",")"," ","#","'","+","-",".","/",":",";","<","=",">","?","[","]","_","`","{","}","|","~"];
 var availableChar = [];
 
 function generatePassword() {
